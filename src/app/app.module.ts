caa7d4e81c01678d9e59d71f242e51c3a1676b21
javakarthicklog <javakarthiklog@gmail.com>
2018-01-16 20:35:49 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HttpModule } from '@angular/http'
+
+import { AboutPage } from '../pages/about/about';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { SettingsPage } from '../pages/setting/setting'
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { weatherService } from '../providers/weather/weather';
+import { FormsModule } from '@angular/forms';
+import { IonicStorageModule } from '@ionic/storage';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    HomePage,
+    TabsPage,
+    SettingsPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    HomePage,
+    TabsPage,
+    SettingsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    weatherService
+  ]
+})
+export class AppModule {}
